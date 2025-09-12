c163422c1d6b569f30514f2fcb6a634b4e7048ea
Christopher Lewis <christopher.lewis@digitalewis.co.uk>
2025-09-12 11:31:00 +0000

    commit

--- models/Gold_Layer/Facts/Questions_Model/Facts_Questions.sql
+++ models/Gold_Layer/Facts/Questions_Model/Facts_Questions.sql
@@ -1,42 +1,61 @@
-{{ config(schema="Gold_layer", materialized="view") }}
-with
-    q as (
-        select
-            question_id,
-            asker_user_id,
-            accepted_answer_id,
-            creation_dt,
-            last_activity_dt,
-            view_count,
-            score,
-            comment_count,
-            favorite_count
-        from {{ ref("Silver_Layer_Questions") }}
-        where question_id is not null
-    ),
-    answers_per_q as (
-        select question_id, count(distinct answer_id) as answer_count_calc
-        from {{ ref("Silver_Layer_Answers") }}
-        group by question_id
-    ),
-    d as (select date_key, date from {{ ref("Dimensions_Date") }})
-select
-    q.question_id,
-    q.asker_user_id,
-    q.accepted_answer_id,
-    q.creation_dt,
-    q.last_activity_dt,
-    q.comment_count,
-    date_trunc(q.creation_dt, month) as creation_month,
-    cd.date_key as creation_date_key,
-    lad.date_key as last_activity_date_key,
-    coalesce(apq.answer_count_calc, 0) as answer_count,
-    q.view_count,
-    q.score,
-    q.favorite_count,
-    (coalesce(apq.answer_count_calc, 0) > 0) as has_answers,
-    (q.accepted_answer_id is not null) as has_accepted_answer
-from q
-left join answers_per_q apq using (question_id)
-left join d cd on cd.date = q.creation_dt
-left join d lad on lad.date = q.last_activity_dt
+{{ config(materialized="table") }}   -- let dbt_project.yaml put this in dbt_gold_layer
+
+WITH q AS (
+  SELECT
+    question_id,
+    asker_user_id,
+    accepted_answer_id,
+    creation_dt,
+    last_activity_dt,
+    view_count,
+    score,
+    comment_count,
+    favorite_count
+  FROM {{ ref('Silver_Layer_Questions') }}
+  WHERE question_id IS NOT NULL
+),
+
+answers_per_q AS (
+  SELECT
+    question_id,
+    COUNT(DISTINCT answer_id) AS answer_count_calc
+  FROM {{ ref('Silver_Layer_Answers') }}
+  GROUP BY question_id
+),
+
+d AS (
+  SELECT date_key, date
+  FROM {{ ref('Dimensions_Date') }}
+)
+
+SELECT
+  q.question_id,
+  q.asker_user_id,
+  q.accepted_answer_id,
+  q.creation_dt,
+  q.last_activity_dt,
+  DATE_TRUNC(q.creation_dt, MONTH) AS creation_month,
+  COALESCE(cd.date_key, 0)  AS creation_date_key,
+  COALESCE(lad.date_key, 0) AS last_activity_date_key,
+  GREATEST(
+    COALESCE(apq.answer_count_calc, 0),
+    CASE WHEN q.accepted_answer_id IS NOT NULL THEN 1 ELSE 0 END
+  ) AS answer_count,
+
+  q.view_count,
+  q.score,
+  q.favorite_count,
+  q.comment_count,
+
+  SAFE_CAST(
+    GREATEST(
+      COALESCE(apq.answer_count_calc, 0),
+      CASE WHEN q.accepted_answer_id IS NOT NULL THEN 1 ELSE 0 END
+    ) > 0 AS BOOL
+  ) AS has_answers,
+
+  SAFE_CAST(q.accepted_answer_id IS NOT NULL AS BOOL) AS has_accepted_answer
+FROM q
+LEFT JOIN answers_per_q apq USING (question_id)
+LEFT JOIN d cd  ON cd.date  = q.creation_dt
+LEFT JOIN d lad ON lad.date = q.last_activity_dt;
