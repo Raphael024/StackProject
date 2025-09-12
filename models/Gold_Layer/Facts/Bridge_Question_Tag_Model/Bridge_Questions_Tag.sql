9f7ae07bc42ef465bd644a61f8b9d71c01b389d9
Christopher Lewis <christopher.lewis@digitalewis.co.uk>
2025-09-12 13:25:41 +0000

    comit

--- models/Gold_Layer/Facts/Bridge_Question_Tag_Model/Bridge_Questions_Tag.sql
+++ models/Gold_Layer/Facts/Bridge_Question_Tag_Model/Bridge_Questions_Tag.sql
@@ -1,18 +1,13 @@
 {{ config(materialized="view") }}
 
 with
-  pairs as (
-    select distinct
-      q.question_id,
-      lower(trim(tag)) as tag
-    from {{ ref("Silver_Layer_Questions") }} q,
-    unnest(coalesce(q.tags_array, ARRAY<STRING>[])) as tag
-    where tag is not null and trim(tag) <> ''
-  )
-
-select distinct
-  p.question_id,
-  d.tag_id
+    pairs as (
+        select distinct q.question_id, lower(trim(tag)) as tag
+        from {{ ref("Silver_Layer_Questions") }} q
+        cross join unnest(coalesce(q.tags_array, array<string>[])) as tag
+        where tag != ''
+    )
+select p.question_id, d.tag_id
 from pairs p
-join {{ ref("Dimensions_Tags") }} d
-  on d.tag = p.tag
+join {{ ref("Dimensions_Tags") }} d using (tag)
+;
